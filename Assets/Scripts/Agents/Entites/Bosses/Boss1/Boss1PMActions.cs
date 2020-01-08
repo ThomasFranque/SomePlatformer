632133a936e99fef41a1ab95b5f6d0426eab55c6
Thomas the Franque <tfranque@gmail.com>
2020-01-08 11:48:26 +0000

    Boss stage 1 almost done

--- Assets/Scripts/Agents/Entites/Bosses/Boss1/Boss1PMActions.cs
+++ Assets/Scripts/Agents/Entites/Bosses/Boss1/Boss1PMActions.cs
@@ -5,26 +5,69 @@ using HutongGames.PlayMaker;
 
 public class Boss1PMActions : Enemy
 {
-	private PlayMakerFSM _bossFsm;
-	private FsmBool _vulnerable;
+	// Use raycasts later to get room size
+	private const float _ROOM_SPACE_TOTAL = 170;
 
-	private byte currentStage = 1; // /3
+	[Header("Boss References")]
+	[SerializeField] private Transform _bossRoomCenter;
+	[Header("Boss Properties")]
+	[UnityEngine.Tooltip("Boss needs to have this health or lower to be on stage 2")]
+	[SerializeField] private uint _stage2RequiredHp = 20;
+	[UnityEngine.Tooltip("Boss needs to have this health or lower to be on stage 3")]
+	[SerializeField] private uint _stage3RequiredHp = 10;
+
+	private PlayMakerFSM _bossFsm;
+	private FsmBool _fsmVulnerable;
+	private FsmBool _fsmPOnRightside;
+	private FsmBool _fsmpHasRoomSpace;
+	private FsmInt _fsmStage;
+
+	private bool IsPlayerOnRightSide => transform.position.x - Player.Instance.transform.position.x < 0;
+
+	private bool HasRoomSpace => transform.position.x < _bossRoomCenter.position.x + (_ROOM_SPACE_TOTAL / 2) && transform.position.x > _bossRoomCenter.position.x - (_ROOM_SPACE_TOTAL / 2);
+
+	private bool SecondStageReached => HP < _stage2RequiredHp;
+	private bool ThirdStageReached => HP < _stage3RequiredHp;
+
+	private uint CurrentStage
+	{
+		get
+		{
+			if (ThirdStageReached) return 3;
+			if (SecondStageReached) return 2;
+			return 1;
+		}
+	}
 
 	protected override void Start()
 	{
 		base.Start();
 		_bossFsm = GetComponent<PlayMakerFSM>();
-		_vulnerable = _bossFsm.FsmVariables.GetFsmBool("Vulnerable");
+		_fsmVulnerable = _bossFsm.FsmVariables.GetFsmBool("Vulnerable");
+		_fsmPOnRightside = _bossFsm.FsmVariables.GetFsmBool("Player on Rightside");
+		_fsmpHasRoomSpace = _bossFsm.FsmVariables.GetFsmBool("Has Room Space");
+		_fsmStage = _bossFsm.FsmVariables.GetFsmInt("Stage");
+		_fsmStage.Value = (int)CurrentStage;
 	}
 
 	protected override void Update()
 	{
 		base.Update();
-		_canBeStomped = _vulnerable.Value;
+		_canBeStomped = _fsmVulnerable.Value;
+		_fsmPOnRightside.Value = IsPlayerOnRightSide;
+		_fsmpHasRoomSpace.Value = HasRoomSpace;
 	}
 
-	public override void KnockBack(bool cameFromRight, float knockSpeed)
+	protected override void OnHit(bool cameFromRight, float knockSpeed, byte dmg)
 	{
-		
+		base.OnHit(cameFromRight, knockSpeed, dmg);
+		UpdateFsmBossStage();
+	}
+
+	public override void KnockBack(bool cameFromRight, float knockSpeed) {}
+
+	private void UpdateFsmBossStage()
+	{
+		_fsmStage.Value = (int)CurrentStage;
 	}
 }
