bd4d33f28d41b6c2ef9f0bc58c846ef9c42b7245
Thomas the Franque <tfranque@gmail.com>
2020-01-26 16:23:27 +0000

    Tweaks to shockwave

--- Assets/Scripts/Agents/Entites/Bosses/Boss1/Deprecated/Boss1.cs
+++ Assets/Scripts/Agents/Entites/Bosses/Boss1/Deprecated/Boss1.cs
@@ -1,185 +1,185 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
 
-public class Boss1 : Enemy
-{
-	private Player _pScript => Player.Instance;
-	private Vector3 _pPos => Player.Instance.transform.position;
+// public class Boss1 : Enemy
+// {
+// 	private Player _pScript => Player.Instance;
+// 	private Vector3 _pPos => Player.Instance.transform.position;
 
-	private Coroutine _currentAttack = null;
-	private Vector3 _targetPosition;
-	private float _yPositionLock;
+// 	private Coroutine _currentAttack = null;
+// 	private Vector3 _targetPosition;
+// 	private float _yPositionLock;
 
-	#region Air Plunge attack
-	[Header("Air Plunge Properties")]
-	[SerializeField] private float _airPlungeUpVelocity = 42.0f;
-	[SerializeField] private float _airPlungeHeight = 24.0f;
-	[SerializeField] private float _airPlungeInAirWait = 2.0f;
-	[SerializeField] private float _airPlungeBeforeDropWait = .5f;
-	[SerializeField] private float _airPlungeMaxWaitForGetNear = 5.0f;
-	[SerializeField] private float _airPlungeDropSpeed= 18.0f;
-	private Vector3 _airPlungeStoredPosition = default;
-	private bool _airPlungeSoftMoveToTargetPos = false;
-	private bool _airPlungeSoftMoveToPlayerX = false;
-	private bool _airPlungeLockYPosition = false;
-	#endregion
+// 	#region Air Plunge attack
+// 	[Header("Air Plunge Properties")]
+// 	[SerializeField] private float _airPlungeUpVelocity = 42.0f;
+// 	[SerializeField] private float _airPlungeHeight = 24.0f;
+// 	[SerializeField] private float _airPlungeInAirWait = 2.0f;
+// 	[SerializeField] private float _airPlungeBeforeDropWait = .5f;
+// 	[SerializeField] private float _airPlungeMaxWaitForGetNear = 5.0f;
+// 	[SerializeField] private float _airPlungeDropSpeed= 18.0f;
+// 	private Vector3 _airPlungeStoredPosition = default;
+// 	private bool _airPlungeSoftMoveToTargetPos = false;
+// 	private bool _airPlungeSoftMoveToPlayerX = false;
+// 	private bool _airPlungeLockYPosition = false;
+// 	#endregion
 
-	private bool SoftMoveToTargetPosition => _airPlungeSoftMoveToTargetPos;
-	private bool SoftMoveToPlayerX => _airPlungeSoftMoveToPlayerX;
-	private bool LockYPosition => _airPlungeLockYPosition;
+// 	private bool SoftMoveToTargetPosition => _airPlungeSoftMoveToTargetPos;
+// 	private bool SoftMoveToPlayerX => _airPlungeSoftMoveToPlayerX;
+// 	private bool LockYPosition => _airPlungeLockYPosition;
 
-	// Start is called before the first frame update
-	protected override void Start()
-    {
-		base.Start();
+// 	// Start is called before the first frame update
+// 	protected override void Start()
+//     {
+// 		base.Start();
 
-		PlungeAttack();
-    }
+// 		PlungeAttack();
+//     }
 
-	// Update is called once per frame
-	protected override void Update()
-    {
-		base.Update();
+// 	// Update is called once per frame
+// 	protected override void Update()
+//     {
+// 		base.Update();
 
-		// AIR PLUNGE
-		if (SoftMoveToTargetPosition)
-			SmoothMoveTowardsTarget(2);
-		if (SoftMoveToPlayerX)
-			SmoothMoveTowardsTarget(3, _pPos, _colliderTouchingSolidGround ? (float?)transform.position.x : null ,_yPositionLock);
-		if (LockYPosition)
-			LockYPos();
-		//
+// 		// AIR PLUNGE
+// 		if (SoftMoveToTargetPosition)
+// 			SmoothMoveTowardsTarget(2);
+// 		if (SoftMoveToPlayerX)
+// 			SmoothMoveTowardsTarget(3, _pPos, _colliderTouchingSolidGround ? (float?)transform.position.x : null ,_yPositionLock);
+// 		if (LockYPosition)
+// 			LockYPos();
+// 		//
 
-		Debug.Log(_colliderTouchingSolidGround);
-	}
+// 		Debug.Log(_colliderTouchingSolidGround);
+// 	}
 
-	private void PlungeAttack()
-	{
-		_currentAttack =  StartCoroutine(CPlungeAttack());
-	}
+// 	private void PlungeAttack()
+// 	{
+// 		_currentAttack =  StartCoroutine(CPlungeAttack());
+// 	}
 
-	private void SetRBVelocity(float x, float y)
-	{
-		rb.velocity = new Vector2(x, y);
-	}
+// 	private void SetRBVelocity(float x, float y)
+// 	{
+// 		rb.velocity = new Vector2(x, y);
+// 	}
 
-	private void SmoothMoveTowardsTarget(float speed, Vector3 target = default, float? xLock = null, float? yLock = null)
-	{
-		if (target == default) target = _targetPosition;
+// 	private void SmoothMoveTowardsTarget(float speed, Vector3 target = default, float? xLock = null, float? yLock = null)
+// 	{
+// 		if (target == default) target = _targetPosition;
 
-		// Preparing variable for Lerp
-		Vector3 _desiredPosition = new Vector3(
-			target.x,
-			target.y,
-			transform.position.z);
-
-
-		// Using Lerp to pan the camera 
-		Vector3 smoothPosition =
-			Vector3.Lerp(transform.position, _desiredPosition,
-			speed * Time.deltaTime);
-
-		if (xLock != null)
-			smoothPosition.x = (float)xLock;
-		if (yLock != null)
-			smoothPosition.y = (float)yLock;
+// 		// Preparing variable for Lerp
+// 		Vector3 _desiredPosition = new Vector3(
+// 			target.x,
+// 			target.y,
+// 			transform.position.z);
 
 
-		transform.position = smoothPosition;
-	}
+// 		// Using Lerp to pan the camera 
+// 		Vector3 smoothPosition =
+// 			Vector3.Lerp(transform.position, _desiredPosition,
+// 			speed * Time.deltaTime);
 
-	private void LockYPos()
-	{
-		transform.position = new Vector3(transform.position.x, _yPositionLock, transform.position.z);
-	}
-
-	private IEnumerator CPlungeAttack()
-	{
-		_airPlungeStoredPosition = transform.position;
-		SetRBVelocity(0, _airPlungeUpVelocity);
-		_useColliderAsTrigger = true;
-		selfCol.isTrigger = true;
-		// Jump UP
-		yield return WaitForAirPlungeReachHeight();
-
-		// Get player pos into _targetpos
-		_targetPosition = new Vector3(_pPos.x, transform.position.y);
-		_yPositionLock = transform.position.y;
-		_airPlungeLockYPosition = true;
-
-		// Wait before moving
-		yield return new WaitForSeconds(_airPlungeInAirWait);
-
-		// Move to gotten pos (set bool to true)
-		rb.gravityScale = 0;
-		_airPlungeLockYPosition = false;
-		_airPlungeSoftMoveToPlayerX = true;
+// 		if (xLock != null)
+// 			smoothPosition.x = (float)xLock;
+// 		if (yLock != null)
+// 			smoothPosition.y = (float)yLock;
 
 
-		_useColliderAsTrigger = false;
-		selfCol.isTrigger = false;
+// 		transform.position = smoothPosition;
+// 	}
 
-		// Move towards player 
-		yield return WaitForAirPlungeAirTimeEnd();
+// 	private void LockYPos()
+// 	{
+// 		transform.position = new Vector3(transform.position.x, _yPositionLock, transform.position.z);
+// 	}
 
-		_useColliderAsTrigger = true;
-		selfCol.isTrigger = true;
+// 	private IEnumerator CPlungeAttack()
+// 	{
+// 		_airPlungeStoredPosition = transform.position;
+// 		SetRBVelocity(0, _airPlungeUpVelocity);
+// 		_useColliderAsTrigger = true;
+// 		selfCol.isTrigger = true;
+// 		// Jump UP
+// 		yield return WaitForAirPlungeReachHeight();
 
-		// Wait before drop
-		yield return new WaitForSeconds(_airPlungeBeforeDropWait);
-		_airPlungeSoftMoveToPlayerX = false;
-		rb.gravityScale = 8;
+// 		// Get player pos into _targetpos
+// 		_targetPosition = new Vector3(_pPos.x, transform.position.y);
+// 		_yPositionLock = transform.position.y;
+// 		_airPlungeLockYPosition = true;
 
-		SetRBVelocity(0, -_airPlungeDropSpeed);
+// 		// Wait before moving
+// 		yield return new WaitForSeconds(_airPlungeInAirWait);
 
-		yield return WaitForTouchGround();
+// 		// Move to gotten pos (set bool to true)
+// 		rb.gravityScale = 0;
+// 		_airPlungeLockYPosition = false;
+// 		_airPlungeSoftMoveToPlayerX = true;
 
-		SetRBVelocity(0, 0);
-		_yPositionLock = transform.position.y;
-		_airPlungeLockYPosition = true;
-		_useColliderAsTrigger = false;
-		selfCol.isTrigger = false;
 
-		yield return new WaitForSeconds(2.0f);
+// 		_useColliderAsTrigger = false;
+// 		selfCol.isTrigger = false;
 
-		_airPlungeLockYPosition = false;
-		_currentAttack = null;
+// 		// Move towards player 
+// 		yield return WaitForAirPlungeAirTimeEnd();
 
-		PlungeAttack();
-	}
+// 		_useColliderAsTrigger = true;
+// 		selfCol.isTrigger = true;
 
-	private Vector3 GetGroundPosition()
-	{
-		RaycastHit2D hit =
-			Physics2D.Raycast(transform.position + _groundCheckOffset, Vector3.down, 600.0f, LayerMask.GetMask("Ground"));
+// 		// Wait before drop
+// 		yield return new WaitForSeconds(_airPlungeBeforeDropWait);
+// 		_airPlungeSoftMoveToPlayerX = false;
+// 		rb.gravityScale = 8;
 
-		Vector3 hitPoint = hit.point;
-		return hitPoint;
-	}
+// 		SetRBVelocity(0, -_airPlungeDropSpeed);
 
-	private IEnumerator WaitForAirPlungeReachHeight()
-	{
-		while (transform.position.y < _airPlungeStoredPosition.y + _airPlungeHeight) yield return null;
-	}
-	private IEnumerator WaitForAirPlungeAirTimeEnd()
-	{
-		float timeOfWaitStart = Time.time;
-		while (transform.position.x - _pPos.x > 0.5f || -0.5 > transform.position.x - _pPos.x || 
-			timeOfWaitStart - Time.time > _airPlungeMaxWaitForGetNear) yield return null;
-	}
-	private IEnumerator WaitForTouchGround()
-	{
-		while (!OnGround) yield return null;
-	}
+// 		yield return WaitForTouchGround();
 
-	protected override void OnTriggerEnterGroundCollision()
-	{
-		base.OnTriggerEnterGroundCollision();
+// 		SetRBVelocity(0, 0);
+// 		_yPositionLock = transform.position.y;
+// 		_airPlungeLockYPosition = true;
+// 		_useColliderAsTrigger = false;
+// 		selfCol.isTrigger = false;
 
-		SetRBVelocity(0, 0);
-		_useColliderAsTrigger = false;
-		selfCol.isTrigger = false;
-		Debug.Log("here");
-	}
-}
+// 		yield return new WaitForSeconds(2.0f);
+
+// 		_airPlungeLockYPosition = false;
+// 		_currentAttack = null;
+
+// 		PlungeAttack();
+// 	}
+
+// 	private Vector3 GetGroundPosition()
+// 	{
+// 		RaycastHit2D hit =
+// 			Physics2D.Raycast(transform.position + _groundCheckOffset, Vector3.down, 600.0f, LayerMask.GetMask("Ground"));
+
+// 		Vector3 hitPoint = hit.point;
+// 		return hitPoint;
+// 	}
+
+// 	private IEnumerator WaitForAirPlungeReachHeight()
+// 	{
+// 		while (transform.position.y < _airPlungeStoredPosition.y + _airPlungeHeight) yield return null;
+// 	}
+// 	private IEnumerator WaitForAirPlungeAirTimeEnd()
+// 	{
+// 		float timeOfWaitStart = Time.time;
+// 		while (transform.position.x - _pPos.x > 0.5f || -0.5 > transform.position.x - _pPos.x || 
+// 			timeOfWaitStart - Time.time > _airPlungeMaxWaitForGetNear) yield return null;
+// 	}
+// 	private IEnumerator WaitForTouchGround()
+// 	{
+// 		while (!OnGround) yield return null;
+// 	}
+
+// 	protected override void OnTriggerEnterGroundCollision()
+// 	{
+// 		base.OnTriggerEnterGroundCollision();
+
+// 		SetRBVelocity(0, 0);
+// 		_useColliderAsTrigger = false;
+// 		selfCol.isTrigger = false;
+// 		Debug.Log("here");
+// 	}
+// }
--- Assets/Scripts/Agents/Entites/Bosses/Boss1/ShockWave.cs
+++ Assets/Scripts/Agents/Entites/Bosses/Boss1/ShockWave.cs
@@ -1,16 +1,28 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class ShockWave : Hazard
 {
 	[SerializeField] private float _speed = 2;
 	[SerializeField] private float _speedIncreaseFactor = 2;
+	[SerializeField] private float _sizeIncreaseFactor = 1.025f;
 
     // Update is called once per frame
     void Update()
     {
+		MoveForward();
+		DecreaseScale();
+	}
+
+	private void DecreaseScale()
+	{
+
+		transform.localScale /= _sizeIncreaseFactor;
+	}
+
+	private void MoveForward()
+	{
 		transform.position += transform.right * _speed * Time.deltaTime;
 		_speed += _speedIncreaseFactor * Time.deltaTime;
+
 	}
 }
